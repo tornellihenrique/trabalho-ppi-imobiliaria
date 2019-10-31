c6954ff26c459b272dbd83194de0011fa57b95b1
Henrique Tornelli Duarte <tornelli.henrique@gmail.com>
2019-10-30 23:10:56 -0300

    atualizando base de dados

--- db.sql
+++ db.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.4
+-- version 4.9.1
 -- https://www.phpmyadmin.net/
 --
--- Host: localhost
--- Tempo de geração: 30/10/2019 às 03:45
--- Versão do servidor: 10.1.37-MariaDB
--- Versão do PHP: 7.3.1
+-- Host: 127.0.0.1
+-- Tempo de geração: 31-Out-2019 às 03:10
+-- Versão do servidor: 10.4.8-MariaDB
+-- versão do PHP: 7.3.10
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -22,25 +22,43 @@ SET time_zone = "+00:00";
 -- Banco de dados: `sweeth00_sweethome`
 --
 DROP DATABASE IF EXISTS `sweeth00_sweethome`;
-CREATE DATABASE IF NOT EXISTS `sweeth00_sweethome` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+CREATE DATABASE IF NOT EXISTS `sweeth00_sweethome` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
 USE `sweeth00_sweethome`;
 
 -- --------------------------------------------------------
 
 --
--- Estrutura para tabela `cargo`
+-- Estrutura da tabela `cargo`
 --
 
 CREATE TABLE IF NOT EXISTS `cargo` (
   `id_cargo` int(2) NOT NULL AUTO_INCREMENT,
   `descricao` varchar(100) DEFAULT NULL,
   PRIMARY KEY (`id_cargo`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
 --
--- Estrutura para tabela `funcionarios`
+-- Estrutura da tabela `clientes`
+--
+
+CREATE TABLE IF NOT EXISTS `clientes` (
+  `cpf` int(11) NOT NULL,
+  `nome` varchar(100) NOT NULL,
+  `endereco` varchar(300) NOT NULL,
+  `telefone` varchar(15) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `sexo` varchar(1) NOT NULL,
+  `estado_civil` varchar(100) NOT NULL,
+  `profissao` varchar(100) NOT NULL,
+  PRIMARY KEY (`cpf`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `funcionarios`
 --
 
 CREATE TABLE IF NOT EXISTS `funcionarios` (
@@ -56,10 +74,10 @@ CREATE TABLE IF NOT EXISTS `funcionarios` (
   `id_cargo` int(2) DEFAULT NULL,
   `salario` double DEFAULT NULL,
   PRIMARY KEY (`usuario`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Despejando dados para a tabela `funcionarios`
+-- Extraindo dados da tabela `funcionarios`
 --
 
 INSERT INTO `funcionarios` (`usuario`, `senha`, `nome`, `telefone`, `cpf`, `endereco`, `telefone_contato`, `telefone_celular`, `data_ingresso`, `id_cargo`, `salario`) VALUES
