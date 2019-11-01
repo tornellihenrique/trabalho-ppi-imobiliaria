6866cd862ba6782354900405622901c9c6305d1b
Henrique Tornelli Duarte <tornelli.henrique@gmail.com>
2019-10-31 21:37:48 -0300

    atualizando base de dados

--- db.sql
+++ db.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.9.1
+-- version 4.8.4
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Tempo de geração: 31-Out-2019 às 03:10
--- Versão do servidor: 10.4.8-MariaDB
--- versão do PHP: 7.3.10
+-- Host: localhost
+-- Tempo de geração: 01/11/2019 às 01:37
+-- Versão do servidor: 10.1.37-MariaDB
+-- Versão do PHP: 7.3.1
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -22,31 +22,43 @@ SET time_zone = "+00:00";
 -- Banco de dados: `sweeth00_sweethome`
 --
 DROP DATABASE IF EXISTS `sweeth00_sweethome`;
-CREATE DATABASE IF NOT EXISTS `sweeth00_sweethome` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+CREATE DATABASE IF NOT EXISTS `sweeth00_sweethome` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
 USE `sweeth00_sweethome`;
 
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `cargo`
+-- Estrutura para tabela `bairros`
+--
+
+CREATE TABLE IF NOT EXISTS `bairros` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nome` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `cargo`
 --
 
 CREATE TABLE IF NOT EXISTS `cargo` (
   `id_cargo` int(2) NOT NULL AUTO_INCREMENT,
   `descricao` varchar(100) DEFAULT NULL,
   PRIMARY KEY (`id_cargo`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `clientes`
+-- Estrutura para tabela `clientes`
 --
 
 CREATE TABLE IF NOT EXISTS `clientes` (
   `cpf` int(11) NOT NULL,
   `nome` varchar(100) NOT NULL,
-  `endereco` varchar(300) NOT NULL,
+  `id_endereco` int(11) NOT NULL,
   `telefone` varchar(15) NOT NULL,
   `email` varchar(100) NOT NULL,
   `sexo` varchar(1) NOT NULL,
@@ -58,7 +70,36 @@ CREATE TABLE IF NOT EXISTS `clientes` (
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `funcionarios`
+-- Estrutura para tabela `enderecos`
+--
+
+CREATE TABLE IF NOT EXISTS `enderecos` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `rua` varchar(200) NOT NULL,
+  `numero` int(11) NOT NULL,
+  `cep` int(8) NOT NULL,
+  `cidade` varchar(100) NOT NULL,
+  `estado_sigla` char(2) NOT NULL,
+  `bairro_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `estados`
+--
+
+CREATE TABLE IF NOT EXISTS `estados` (
+  `sigla` char(2) NOT NULL,
+  `nome` varchar(100) NOT NULL,
+  PRIMARY KEY (`sigla`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `funcionarios`
 --
 
 CREATE TABLE IF NOT EXISTS `funcionarios` (
@@ -67,20 +108,20 @@ CREATE TABLE IF NOT EXISTS `funcionarios` (
   `nome` varchar(100) DEFAULT NULL,
   `telefone` varchar(100) DEFAULT NULL,
   `cpf` varchar(14) DEFAULT NULL,
-  `endereco` varchar(100) DEFAULT NULL,
+  `id_endereco` int(11) DEFAULT NULL,
   `telefone_contato` varchar(14) DEFAULT NULL,
   `telefone_celular` varchar(15) DEFAULT NULL,
   `data_ingresso` date DEFAULT NULL,
   `id_cargo` int(2) DEFAULT NULL,
   `salario` double DEFAULT NULL,
   PRIMARY KEY (`usuario`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Extraindo dados da tabela `funcionarios`
+-- Despejando dados para a tabela `funcionarios`
 --
 
-INSERT INTO `funcionarios` (`usuario`, `senha`, `nome`, `telefone`, `cpf`, `endereco`, `telefone_contato`, `telefone_celular`, `data_ingresso`, `id_cargo`, `salario`) VALUES
+INSERT INTO `funcionarios` (`usuario`, `senha`, `nome`, `telefone`, `cpf`, `id_endereco`, `telefone_contato`, `telefone_celular`, `data_ingresso`, `id_cargo`, `salario`) VALUES
 ('admin', '$2y$10$84ITFwt8Nw8PDDqytuemb.I45kv8GAG1or9pImqZBI36L.OMYA0Iy', NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
 COMMIT;
 
